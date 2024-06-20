b37a2e0d67b17e6be40d9ddd9f654e5105b09d23
Roy Quesada <roy.quesada@gmail.com>
2024-06-19 18:26:26 -0600

    Create share modal

--- src/components/deleteMessage/deleteConfirmationModal.tsx
+++ src/components/deleteMessage/deleteConfirmationModal.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Modal from "react-modal";
-import "./confirmationModal.css"; // Reuse the existing styles or create new ones
+import PrimaryButton from "../../components/buttons/primaryButton/primaryButton";
+import SecondaryButton from "../../components/buttons/secondaryButton/secondaryButton";
+
+import "./deleteConfirmationModal.css"; // Reuse the existing styles or create new ones
 
 interface DeleteConfirmationModalProps {
   isOpen: boolean;
@@ -17,8 +20,8 @@ const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} className="modal" overlayClassName="overlay">
       <h2>Are you sure you want to delete this bot?</h2>
       <div className="modal-buttons">
-        <button onClick={onRequestClose} className="modal-button">No</button>
-        <button onClick={onConfirm} className="modal-button">Yes</button>
+        <SecondaryButton text="Delete" onClick={onConfirm} enabled={true} />
+        <PrimaryButton text="Cancel" onClick={onRequestClose} enabled={true} />
       </div>
     </Modal>
   );
