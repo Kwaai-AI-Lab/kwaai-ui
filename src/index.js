308b6c23c2b4f692bb03143a206721bec6b38e5a
Roy Quesada <roy.quesada@gmail.com>
2024-09-11 12:13:11 -0600

    Remove passkey

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './context/AuthContext.tsx'; // Import AuthProvider
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider> {/* Wrap App with AuthProvider */}
       <App />
-    </AuthProvider>
   </React.StrictMode>
 );
 
--- src/pages/navBar/navbar.tsx
+++ src/pages/navBar/navbar.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import ProfileDropdown from "../../components/profileDropdown/profileDropdown";
 import "./navbar.css";
 
 const NavBar: React.FC = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-menu">
-        <ProfileDropdown />
       </ul>
     </nav>
   );
