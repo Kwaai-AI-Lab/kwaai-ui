965408c9587bfc5ed2bee1cefd7412d96a39cde1
Roy Quesada <roy.quesada@gmail.com>
2024-10-24 13:12:48 -0600

    Fixes in JWT

--- src/utils/auth.helper.ts
+++ src/utils/auth.helper.ts
@@ -1,6 +1,6 @@
 // auth.helper.ts
 export const getAuthToken = (): string => {
     // For now, return a dummy token. Later, replace this with actual logic to retrieve the token from localStorage or a context.
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MWJkN2ExZi05NzlkLTQxNDMtYjZhZi1jZjYwYzdmMzNjZjkiLCJpYXQiOjE3Mjk2NDMzNTMsImV4cCI6MTcyOTcyOTc1M30.EGKXPavj36xf-P2wxtIEE5-ZShx-fFpENLl3MvlQ090";
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0Yjk0YjNiZC1mMmY4LTQwMTYtOWI2ZS05ZGJlMDNlMTg0MDAiLCJpYXQiOjE3Mjk3OTExNjgsImV4cCI6MTcyOTg3NzU2OH0.flBxwuSDQWOl_0tNHqYufjAuKQU6zmOy9UBo_0EgSr4";
   };
   
